c66f83ab1257f617c9a90c031c5f7c932f1d2134
akash-14753 <akash.sh@zohocorp.com>
2024-06-25 00:57:29 +0530

    login exception handled

--- src/main/java/com/akash/CareSync/security/CustomUserDetailsService.java
+++ src/main/java/com/akash/CareSync/security/CustomUserDetailsService.java
@@ -1,7 +1,10 @@
 package com.akash.CareSync.security;
 
+import com.akash.CareSync.config.AppConstants;
 import com.akash.CareSync.practicemember.entity.PracticeMember;
 import com.akash.CareSync.practicemember.repository.PracticeMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,10 +12,14 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private AppConstants appConstants;
 
     private final PracticeMemberRepository practiceMemberRepository;
 
@@ -22,7 +29,11 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String user_name) throws UsernameNotFoundException {
-        PracticeMember member = this.practiceMemberRepository.findByUserName(user_name).orElseThrow(null);
+        Optional<PracticeMember> optionalMember = this.practiceMemberRepository.findByUserName(user_name);
+        if(optionalMember.isEmpty()){
+            throw new UsernameNotFoundException(appConstants.USER_NOT_FOUND);
+        }
+        PracticeMember member = optionalMember.get();
         return new User(member.getUsername(), member.getPassword(), member.getAuthorities());
     }
 }
